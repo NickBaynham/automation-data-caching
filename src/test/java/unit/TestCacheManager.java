26a956b55a4979fcef02a2db7b9b8acebd1f83b6
FLI\nbaynham <nbaynham@flhi.com>
2020-01-05 14:02:19 -0500

    unit tests for cache manager

--- src/test/java/unit/TestCacheManager.java
+++ src/test/java/unit/TestCacheManager.java
@@ -7,19 +7,8 @@ import static org.junit.Assert.*;
 public class TestCacheManager {
 
     @Test
-    public void testCacheManager() {
-        Cache cache = CacheManager.getInstance("cacheExample");
-        assertEquals(cache.getId(), "cacheExample");
-
-        // TODO: add an item to the cache
-        cache.add("item1", "value1");
-
-        // TODO: Retrive an item from the cache
-
-        // TODO: Remove an item from the cache
-
-        // TODO: Clear Cache
-
-        // TODO: Retrieve Usage Report on Cache
+    public void verifyGetInstanceOfCacheFromCacheManager() {
+        Cache cache = CacheManager.getInstance("CacheManager");
+        assertEquals(cache.getId(), "CacheManager");
     }
 }
